2720ef5e84ddda40b956935626ec058d359b569b
eNDO9 <ndo@isdglobal.org>
2024-11-06 10:58:09 -0500

    ~~~
    ~~~

--- .ipynb_checkpoints/Link-checkpoint.py
+++ .ipynb_checkpoints/Link-checkpoint.py
@@ -29,7 +29,8 @@ def main():
 
             # Print notifications
             st.write("**Source** and **Target** columns selected.")
-            st.write(f"**Source:** {source_column}\n**Target:** {target_column}")
+            st.write(f"**Source:** {source_column}")
+            st.write(f"**Target:** {target_column}")
 
             # Save df in session state for use in later steps
             st.session_state['df'] = df
