4ebb65f0a57208eab6973404b2673aea8c5458ce
eNDO9 <ndo@isdglobal.org>
2024-11-30 19:54:11 -0500

    logo fix

--- Link.py
+++ Link.py
@@ -8,7 +8,7 @@ from urllib.parse import urlparse
 def main():
     st.title("Link")
     # Add a logo
-    st.image("logo.png", width=150)
+    st.logo("logo.png", width=150)
     
     st.markdown("<p style='font-size:20px; font-style: italic;'>This tool creates a network graph from imported CSVs.</p>", unsafe_allow_html=True)
     st.write("")
