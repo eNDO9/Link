cd9df959cf2280e3fb170de9698a9b13e199727a
eNDO9 <ndo@isdglobal.org>
2024-12-02 10:30:31 -0500

    user guide test - via GitHub

--- .ipynb_checkpoints/Link-checkpoint.py
+++ .ipynb_checkpoints/Link-checkpoint.py
@@ -13,6 +13,18 @@ def main():
     st.markdown("<p style='font-size:20px; font-style: italic;'>This tool creates a network graph from imported CSVs.</p>", unsafe_allow_html=True)
     st.write("")
     st.write("")
+    
+    st.markdown("### User Guide")
+    st.markdown(
+    """
+    ### User Guide
+    Need help? [View the User Guide](https://github.com/eNDO9/Link/blob/main/Overview%20-%20Link.pdf) (opens in a new tab).
+    """,
+    unsafe_allow_html=True
+    )
+    
+    st.write("")
+    st.write("")
     st.subheader("Step 1: Upload and Process CSVs")
 
     # Step 1: Upload and Process CSVs
